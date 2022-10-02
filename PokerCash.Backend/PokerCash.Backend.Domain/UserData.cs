115d654c989a7a4fcb8e337dc2d28125e8dbf29b
Fajo <lalkindim@yandex.ru>
2022-10-03 01:51:50 +0300

    user classes added

---
+++ PokerCash.Backend/PokerCash.Backend.Domain/UserData.cs
@@ -0,0 +1,32 @@
+﻿namespace PokerCash.Backend.Domain;
+
+/// <summary>
+/// Basic data user
+/// </summary>
+public class UserData
+{
+    /// <summary>
+    /// Unique id user data
+    /// </summary>
+    public Guid Id { get; set; }
+    /// <summary>
+    /// Basic user statistics
+    /// </summary>
+    public UserStatistics UserStatistics { get; set; }
+    /// <summary>
+    /// User login from the authorization server
+    /// </summary>
+    public string Login { get; set; }
+    /// <summary>
+    /// Free user chips 
+    /// </summary>
+    public decimal Cash { get; set; }
+    /// <summary>
+    /// User blocking
+    /// </summary>
+    public bool IsBanned { get; set; }
+    /// <summary>
+    /// Date of last receipt of free chips
+    /// </summary>
+    public DateTime LastTakeCash { get; set; }
+}
---
+++ PokerCash.Backend/PokerCash.Backend.Domain/UserStatistics.cs
@@ -0,0 +1,24 @@
+﻿namespace PokerCash.Backend.Domain;
+
+/// <summary>
+/// Basic user statistics
+/// </summary>
+public class UserStatistics
+{
+    /// <summary>
+    /// Unique id user statistics
+    /// </summary>
+    public Guid Id { get; set; }
+    /// <summary>
+    /// Basic data user
+    /// </summary>
+    public UserData UserData { get; set; }
+    /// <summary>
+    /// Number of games played
+    /// </summary>
+    public int GameCount { get; set; }
+    /// <summary>
+    /// Number of hands won
+    /// </summary>
+    public int HandsWon { get; set; }
+}
