cdf70b51e64a4e5d17449369ff0148d735a9fb09
Fajo <lalkindim@yandex.ru>
2022-10-05 00:59:34 +0300

    hand power class/test created (demo)

--- PokerCash.Backend/PokerCash.Backend.SignalR/GameCore/HandPower.cs
+++ PokerCash.Backend/PokerCash.Backend.SignalR/GameCore/HandPower.cs
@@ -1,6 +1,55 @@
-﻿namespace PokerCash.Backend.SignalR.GameCore;
+﻿using PokerCash.Backend.Utils;
+using System.Linq;
+
+namespace PokerCash.Backend.SignalR.GameCore;
 
 public static class HandPower
 {
-    
+    public static Power GetPower(List<Card> cards)
+    {
+        var hand = cards.OrderBy(c => c.Rank).ToList();
+
+        if (RoyalFlush(hand)) return Power.RoyalFlush;
+        if (StraightFlush(hand)) return Power.StraightFlush;
+        if (FourOfAKindCheck(hand)) return Power.FourOfAKind;
+        if (FullHouseCheck(hand)) return Power.FullHouse;
+        if (FlushCheck(hand)) return Power.Flush;
+        if (StraightCheck(hand)) return Power.Straight;
+        if (ThreeOfAKindCheck(hand)) return Power.ThreeOfAKind;
+        if (TwoPairCheck(hand)) return Power.TwoPair;
+        if (OnePairCheck(hand)) return Power.OnePair;
+
+        return Power.High;
+    }
+
+    private static bool OnePairCheck(List<Card> hand) => hand.GroupBy(h => h.Rank).Count(h => h.Count() == 2) == 1;
+    private static bool TwoPairCheck(List<Card> hand) => hand.GroupBy(h => h.Rank).Count(h => h.Count() == 2) > 1;
+    private static bool ThreeOfAKindCheck(List<Card> hand) => hand.GroupBy(h => h.Rank).Count(h => h.Count() == 3) > 0;
+    private static bool StraightCheck(List<Card> hand)
+    {
+        if (hand[^1].Rank == Rank.Ace && hand[0].Rank == Rank.Two && hand[1].Rank == Rank.Three &&
+            hand[2].Rank == Rank.Four && hand[3].Rank == Rank.Five) return true;
+
+        var h = string.Concat(hand.OrderBy(c => c.Rank).Select(c => (int)c.Rank).Distinct());
+        var straights = new[] { "23456", "34567", "45678", "56789", "678910", "7891011", "89101112", "910111213", "1011121314" };
+        return straights.Select(s => h.Contains(s)).Contains(true);
+    }
+    private static bool FlushCheck(List<Card> hand) => hand.GroupBy(h => h.Suit).Count(h => h.Count() == 5) == 1;
+    private static bool FullHouseCheck(List<Card> hand)
+    {
+        var group = hand.GroupBy(h => h.Rank);
+        var three = group.Count(t => t.Count() == 3) > 0;
+        var threeX2 = group.Count(t => t.Count() == 3) == 2;
+        var two = group.Count(t => t.Count() == 2) > 0;
+        return (three && two) || threeX2;
+    }
+
+    private static bool FourOfAKindCheck(List<Card> hand) => hand.GroupBy(h => h.Rank).Count(h => h.Count() == 4) == 1;
+    private static bool StraightFlush(List<Card> hand) => StraightCheck(hand) && FlushCheck(hand);
+    private static bool RoyalFlush(List<Card> hand) =>    hand[^1].Rank == Rank.Ace 
+                                                       && hand[^2].Rank == Rank.King
+                                                       && hand[^3].Rank == Rank.Queen
+                                                       && hand[^4].Rank == Rank.Jack
+                                                       && hand[^5].Rank == Rank.Ten
+                                                       && FlushCheck(hand);
 }
---
+++ PokerCash.Backend/PokerCash.GameCoreTests/HandPowerTests.cs
@@ -0,0 +1,229 @@
+﻿using PokerCash.Backend.SignalR.GameCore;
+using PokerCash.Backend.SignalR.GameCore.GameLib;
+using PokerCash.Backend.SignalR.GameCore.PlayerLib;
+using PokerCash.Backend.Utils;
+
+namespace PokerCash.GameCoreTests;
+
+public class HandPowerTests
+{
+    [Fact]
+    public void HighCardCheck_Success()
+    {
+        var expect = Power.High;
+        var cards = new List<Card>
+        {
+            new(Rank.Ten, Suit.Club),
+            new(Rank.Ace, Suit.Diamond),
+            new(Rank.Two, Suit.Club),
+            new(Rank.Seven, Suit.Club),
+            new(Rank.Eight, Suit.Heart),
+            new(Rank.King, Suit.Heart),
+            new(Rank.Three, Suit.Diamond)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void OnePairCheck_Success()
+    {
+        var expect = Power.OnePair;
+        var cards = new List<Card>
+        {
+            new(Rank.Ace, Suit.Club),
+            new(Rank.Ace, Suit.Diamond),
+            new(Rank.Two, Suit.Club),
+            new(Rank.Seven, Suit.Club),
+            new(Rank.Eight, Suit.Heart),
+            new(Rank.King, Suit.Heart),
+            new(Rank.Three, Suit.Diamond)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void TwoPairCheck_Success()
+    {
+        var expect = Power.TwoPair;
+        var cards = new List<Card>
+        {
+            new(Rank.Ace, Suit.Club),
+            new(Rank.Ace, Suit.Diamond),
+            new(Rank.Two, Suit.Club),
+            new(Rank.Seven, Suit.Club),
+            new(Rank.Eight, Suit.Heart),
+            new(Rank.King, Suit.Heart),
+            new(Rank.Two, Suit.Diamond)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void ThreeOfAKindCheck_Success()
+    {
+        var expect = Power.ThreeOfAKind;
+        var cards = new List<Card>
+        {
+            new(Rank.Ace, Suit.Club),
+            new(Rank.Ace, Suit.Diamond),
+            new(Rank.Two, Suit.Club),
+            new(Rank.Seven, Suit.Club),
+            new(Rank.Eight, Suit.Heart),
+            new(Rank.King, Suit.Heart),
+            new(Rank.Ace, Suit.Spade)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void StraightWheelCheck_Success()
+    {
+        var expect = Power.Straight;
+        var cards = new List<Card>
+        {
+            new(Rank.Ace, Suit.Club),
+            new(Rank.Queen, Suit.Diamond),
+            new(Rank.Two, Suit.Club),
+            new(Rank.Eight, Suit.Club),
+            new(Rank.Five, Suit.Heart),
+            new(Rank.Four, Suit.Heart),
+            new(Rank.Three, Suit.Spade)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void StraightCheck_Success()
+    {
+        var expect = Power.Straight;
+        var cards = new List<Card>
+        {
+            new(Rank.King, Suit.Club),
+            new(Rank.Queen, Suit.Diamond),
+            new(Rank.Jack, Suit.Club),
+            new(Rank.Ten, Suit.Club),
+            new(Rank.Ace, Suit.Heart),
+            new(Rank.Four, Suit.Heart),
+            new(Rank.Ace, Suit.Spade)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void FlushCheck_Success()
+    {
+        var expect = Power.Flush;
+        var cards = new List<Card>
+        {
+            new(Rank.Ace, Suit.Diamond),
+            new(Rank.Two, Suit.Diamond),
+            new(Rank.Five, Suit.Club),
+            new(Rank.Ten, Suit.Club),
+            new(Rank.Four, Suit.Diamond),
+            new(Rank.Six, Suit.Diamond),
+            new(Rank.Seven, Suit.Diamond)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void FullHouseCheck_Success()
+    {
+        var expect = Power.FullHouse;
+        var cards = new List<Card>
+        {
+            new(Rank.Two, Suit.Spade),
+            new(Rank.Jack, Suit.Club),
+            new(Rank.Eight, Suit.Heart),
+            new(Rank.Six, Suit.Club),
+            new(Rank.Jack, Suit.Club),
+            new(Rank.Six, Suit.Diamond),
+            new(Rank.Six, Suit.Diamond)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void FourOfAKindCheck_Success()
+    {
+        var expect = Power.FourOfAKind;
+        var cards = new List<Card>
+        {
+            new(Rank.Six, Suit.Spade),
+            new(Rank.Jack, Suit.Club),
+            new(Rank.Eight, Suit.Heart),
+            new(Rank.Six, Suit.Club),
+            new(Rank.Jack, Suit.Club),
+            new(Rank.Six, Suit.Heart),
+            new(Rank.Six, Suit.Diamond)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void StraightFlushCheck_Success()
+    {
+        var expect = Power.StraightFlush;
+        var cards = new List<Card>
+        {
+            new(Rank.Ace, Suit.Club),
+            new(Rank.Eight, Suit.Club),
+            new(Rank.Five, Suit.Club),
+            new(Rank.Ten, Suit.Club),
+            new(Rank.Four, Suit.Heart),
+            new(Rank.Six, Suit.Diamond),
+            new(Rank.Seven, Suit.Club)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+
+    [Fact]
+    public void RoyalFlushCheck_Success()
+    {
+        var expect = Power.RoyalFlush;
+        var cards = new List<Card>
+        {
+            new(Rank.Ace, Suit.Club),
+            new(Rank.King, Suit.Club),
+            new(Rank.Five, Suit.Heart),
+            new(Rank.Queen, Suit.Club),
+            new(Rank.Jack, Suit.Club),
+            new(Rank.Six, Suit.Diamond),
+            new(Rank.Ten, Suit.Club)
+        };
+
+        var actual = HandPower.GetPower(cards);
+
+        Assert.Equal(expect, actual);
+    }
+}
